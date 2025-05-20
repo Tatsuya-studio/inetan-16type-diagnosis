6634406ab99f1721df9de4e07a8008950cc2c3f4
Tatsuya-studio <inuneko.studio.lab@gmail.com>
2025-05-20 13:56:33 +0900

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,9 +2,16 @@ module.exports = {
   content: [
     "./pages/**/*.{js,ts,jsx,tsx}",
     "./components/**/*.{js,ts,jsx,tsx}",
+    "./styles/**/*.{js,ts,jsx,tsx,css}",
+    "./data/**/*.{js,ts,jsx,tsx}"
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        tan: "#f4a261",
+        tanHover: "#e57d23"
+      }
+    }
   },
   plugins: [],
-}
+};
