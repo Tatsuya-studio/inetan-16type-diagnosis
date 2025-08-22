a637a7a345ae279f9466ff76826db9ae0af7afb4
Tatsuya-studio <inuneko.studio.lab@gmail.com>
2025-08-22 13:20:05 +0900

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,33 @@
 // pages/_app.js
 import '../styles/globals.css';
+import Script from 'next/script';
+
+const GA_ID = process.env.NEXT_PUBLIC_GA_ID;
 
 export default function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      {/* GA4 */}
+      {GA_ID && (
+        <>
+          <Script
+            src={`https://www.googletagmanager.com/gtag/js?id=${GA_ID}`}
+            strategy="afterInteractive"
+          />
+          <Script id="ga4-init" strategy="afterInteractive">
+            {`
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', '${GA_ID}', {
+                linker: { domains: ['inunekotype.jp', 'inetan-16type-diagnosis.vercel.app'] }
+              });
+            `}
+          </Script>
+        </>
+      )}
+
+      <Component {...pageProps} />
+    </>
+  );
 }
